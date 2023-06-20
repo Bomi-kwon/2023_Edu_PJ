d37e20aac30a40953a64c82c1070ac51b3eabcdf
Bomi-kwon <bbomicoding@gmail.com>
2023-06-20 13:49:04 +0900

    baeldung 튜토리얼 따라함 - 실패

--- src/main/java/com/koreaIT/project/controller/ProjectRtcController.java
+++ src/main/java/com/koreaIT/project/controller/ProjectRtcController.java
@@ -3,6 +3,7 @@ package com.koreaIT.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.koreaIT.project.dto.WebSocketMessage;
 import com.koreaIT.project.service.RtcChatService;
@@ -17,6 +18,14 @@ public class ProjectRtcController {
         // log.info("MESSAGE : {}", webSocketMessage.toString());
         return Boolean.toString(rtcChatService.findUserCount(webSocketMessage));
     }
+    
+    
+	// 화상채팅
+	
+	@RequestMapping("/project/webrtc/WebRTC")
+	public String WebRTC() {
+		return "project/webrtc/WebRTC";
+	}
 	
 	
 }
