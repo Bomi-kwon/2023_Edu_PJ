c2f7b91ae18a456f2821dc656cd4e9b24c4418f0
Bomi-kwon <bbomicoding@gmail.com>
2023-06-23 17:56:28 +0900

    스크린샷 공유 기능 추가

--- target/classes/static/js/rtc/webrtc_client.js
+++ target/classes/static/js/rtc/webrtc_client.js
@@ -17,6 +17,10 @@ const remoteVideo = document.getElementById('remote_video');
 // const localUserName = localStorage.getItem("uuid");
 const localUserName = document.querySelector("#uuid").value
 
+document.querySelector('#view_on').addEventListener('click', startScreenShare);
+document.querySelector('#view_off').addEventListener('click', stopScreenShare);
+
+
 // WebRTC STUN servers
 // WebRTC STUN 서버 정보
 const peerConnectionConfig = {
@@ -282,6 +286,7 @@ function createPeerConnection() {
 
     // the following events are optional and could be realized later if needed
     // myPeerConnection.onremovetrack = handleRemoveTrackEvent;
+    
     myPeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
     // myPeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
     // myPeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
@@ -440,3 +445,138 @@ function handleNewICECandidateMessage(message) {
     console.log("Adding received ICE candidate: " + JSON.stringify(candidate));
     myPeerConnection.addIceCandidate(candidate).catch(handleErrorMessage);
 }
+
+/*  화면 공유 */
+const screenHandler = new ScreenHandler();
+let shareView = null;
+
+/**
+ * ScreenHandler
+ * @constructor
+ */
+function ScreenHandler() {
+    // let localStream = null;
+
+    log('Loaded ScreenHandler', arguments);
+
+    // REF https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints#Properties_of_shared_screen_tracks
+    const constraints = {
+        audio: true,
+        video: {
+            width: 1980, // 최대 너비
+            height: 1080, // 최대 높이
+            frameRate: 40, // 최대 프레임
+        },
+    };
+
+    /**
+     * 스크린캡쳐 API를 브라우저 호환성 맞게 리턴합니다.
+     * navigator.mediaDevices.getDisplayMedia 호출 (크롬 72 이상 지원)
+     * navigator.getDisplayMedia 호출 (크롬 70 ~ 71 실험실기능 활성화 or Edge)
+     */
+    function getCrossBrowserScreenCapture() {
+        if (navigator.getDisplayMedia) {
+            return navigator.getDisplayMedia(constraints);
+        } else if (navigator.mediaDevices.getDisplayMedia) {
+            return navigator.mediaDevices.getDisplayMedia(constraints);
+        }
+    }
+
+    /**
+     * 스크린캡쳐 API를 호출합니다.
+     * @returns shareView
+     */
+    async function start() {
+
+        try {
+            shareView = await getCrossBrowserScreenCapture();
+        } catch (err) {
+            log('Error getDisplayMedia', err);
+        }
+
+        return shareView;
+    }
+
+    /**
+     * 스트림의 트렉을 stop()시켜 스트림이 전송을 중지합니다.
+     */
+    function end() {
+
+        shareView.getTracks().forEach((track) => {
+            // log("화면 공유 중지")
+            track.stop();
+        });
+
+        // 전송 중단 시 share-video 부분 hide
+        // $("#share-video").hide();
+    }
+
+    /**
+     * extends
+     */
+    this.start = start;
+    this.end = end;
+
+}
+
+/**
+ * 로컬 스트림 핸들링
+ * @param stream
+ */
+function onLocalStream(stream) {
+    log('onLocalStream', stream);
+
+    const $video = document.querySelector('#share-video');
+    $video.srcObject = stream;
+}
+
+/**
+ * screenHandler를 통해 스크린 API를 호출합니다 - 시작
+ * 원격 화면을 화면 공유 화면으로 교체
+ */
+async function startScreenShare() {
+	
+	// 스크린 API 호출 & 시작
+    await screenHandler.start();
+    
+     // 1. myPeerConnection 에 연결된 다른 sender 쪽으로 - 즉 다른 Peer 쪽으로 -
+    // 2. shareView 의 Track 에서 0번째 인덱스에 들어있는 값 - 즉 videoStream 로 - 교체한다.
+    await myPeerConnection.getSenders().forEach((sender)=>{ // 연결된 sender 로 보내기위한 반복문
+
+    // 3. track 를 shareView 트랙으로 교체
+    sender.replaceTrack(shareView.getTracks()[0])
+    })
+	
+
+    /**
+     * 화면 공유 중지 눌렀을 때 이벤트
+     * share-video 가 사라지는 것 뿐만 아니라 추가로 video-off 에서 video-on 으로 변경되도록 만들어야함
+     */
+    shareView.getVideoTracks()[0].addEventListener('ended', () =>{
+        // log('screensharing has ended')
+
+        // 4. 화면 공유 중지 시 Track 를 localstream 의 videoStram 로 교체함
+        myPeerConnection.getSenders().forEach((sender) =>{
+            sender.replaceTrack(localStream.getTracks()[1]);
+        })
+
+        // $("#share-video").hide();
+
+    });
+
+}
+
+/*
+*  video off 버튼을 통해 스크린 API 종료
+* */
+async function stopScreenShare(){
+    // screen share 종료
+    await screenHandler.end();
+    // myPeerConnection
+    await myPeerConnection.getSenders().forEach((sender) =>{
+        
+    // 4. 화면 공유 중지 시 Track 를 localstream 의 videoStram 로 교체함
+    sender.replaceTrack(localStream.getTracks()[1]);
+    })
+}
+
