224f9dcd6c1cae738dfd0172b1048ba7a69b7b1e
Bomi-kwon <bbomicoding@gmail.com>
2023-07-04 15:50:21 +0900

    셀레니움 컨트롤러 수정

--- src/main/java/com/koreaIT/project/controller/ProjectSeleniumController.java
+++ src/main/java/com/koreaIT/project/controller/ProjectSeleniumController.java
@@ -122,63 +122,6 @@ public class ProjectSeleniumController {
 		return "project/home/entranceinfo";
 	}
 	
-	/*
-	 * url 눌러서 이동한 웹페이지의 내용도 긁어와서
-	 * 내 웹사이트에서 그 detail을 볼 수 있도록 만드려고 했으나
-	 * 아직 미완성이다!!
-	@RequestMapping("/project/selenium/entranceinfoDatail")
-	public String entranceinfoDetail(Model model, String href) throws InterruptedException {
-		
-		url = href;
-
-		System.setProperty("webdriver.chrome.driver", path.toString());
-
-		ChromeOptions options = new ChromeOptions();
-
-		
-		options.addArguments("--disable-popup-blocking");   // 팝업 안띄움
-        options.addArguments("headless");   // 브라우저 안띄움
-        options.addArguments("--disable-gpu");  // gpu 비활성화
-        options.addArguments("--blink-settings=imagesEnabled=false");   // 이미지 다운 안받음
-        // options.setExperimentalOption("debuggerAddress", "localhost:9222");
-		options.addArguments("--remote-allow-origins=*");
-
-		WebDriver driver = new ChromeDriver(options);
-
-		//WebDriver을 해당 url로 이동한다.
-		driver.get(url);
-		
-		Thread.sleep(1000);
-		
-		// iframe 태그에 갇혀있는 요소들 데려오려면 거기로 switch 해줘야함!!
-
-		
-		
-		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe#cafe_main")));
-		
-		WebElement body = driver.findElement(By.cssSelector("#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div:nth-child(2) > div.content.CafeViewer > div > div.se-main-container"));
-		
-		List<WebElement> texts = body.findElements(By.cssSelector("div.se-text > div > div > div > p > span"));
-		
-		List<String> bodyList = new ArrayList<>();
-		
-		for(WebElement text : texts) {
-			bodyList.add(text.getText());
-		}
-		
-		if(bodyList.isEmpty()) {
-			rq.jsReturnOnView("웹페이지에서 찾아온 결과가 없습니다.", true);
-		}
-		
-		model.addAttribute("bodyList", bodyList);
-		
-		driver.quit();
-		
-		
-
-		return "project/home/entranceinfoDetail";
-	}
-	*/
 	
 	
 }
